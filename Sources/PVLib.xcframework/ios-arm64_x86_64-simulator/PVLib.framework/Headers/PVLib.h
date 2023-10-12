8446a9921e9a25a04d0ac3b77e5b74b9b2afd92f
Nezhdanov Demian <nzhd.demian@gmail.com>
2023-10-12 01:40:22 -0300

    jg

---
+++ Sources/PVLib.xcframework/ios-arm64_x86_64-simulator/PVLib.framework/Headers/PVLib.h
@@ -0,0 +1,18 @@
+//
+//  PVLib.h
+//  PVLib
+//
+//  Created by Demian Nezhdanov on 28/09/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PVLib.
+FOUNDATION_EXPORT double PVLibVersionNumber;
+
+//! Project version string for PVLib.
+FOUNDATION_EXPORT const unsigned char PVLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PVLib/PublicHeader.h>
+
+
